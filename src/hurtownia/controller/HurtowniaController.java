9a2b581540afaa20fc84fe8a59f8bf871eb3b5c4
kompanpawel <pawelmiskiewicz14@wp.pl>
2018-05-19 15:00:09 +0200

    v0.2

--- src/hurtownia/controller/HurtowniaController.java
+++ src/hurtownia/controller/HurtowniaController.java
@@ -1,5 +1,6 @@
 package hurtownia.controller;
 
+import com.mysql.cj.xdevapi.SqlDataResult;
 import hurtownia.dbUtil.dbConnection;
 import hurtownia.model.NapojeModel;
 import hurtownia.model.SokiModel;
@@ -25,6 +26,9 @@ public class HurtowniaController {
 
     private dbConnection dc;
     private ObservableList<SokiModel> sokiList;
+    private ObservableList<NapojeModel> napojeList;
+    private ObservableList<ZamSokiModel> zamSokiList;
+    private ObservableList<ZamNapojeModel> zamNapojeList;
 
 
     @FXML
@@ -91,18 +95,27 @@ public class HurtowniaController {
 
 
     private String sqlSoki = "SELECT * FROM mydb.soki";
+    private String sqlNapoje = "SELECT * FROM mydb.napoje";
+    private String sqlZamSoki = "SELECT k.Imię,k.Nazwisko,sk.Nazwa,s.Ilość,(s.Ilość * sk.Cena) FROM mydb.klienci k JOIN mydb.zamówienia z ON k.ID_klienta = z.Klienci_ID_klienta JOIN mydb.zamówienia_has_soki s ON z.ID_zamówienia = s.`Zamówienia_ID.zamówienia` JOIN mydb.soki sk ON s.`Soki_ID.soku` = sk.`ID.soku`";
+    private String sqlZamNapoje = "SELECT k.Imię,k.Nazwisko,np.Nazwa,n.Ilość,(n.Ilość * np.Cena) FROM mydb.klienci k \n" +
+            "\tJOIN mydb.zamówienia z ON k.ID_klienta = z.Klienci_ID_klienta \n" +
+            "\tJOIN mydb.zamówienia_has_napoje n ON z.ID_zamówienia = n.`Zamówienia_ID.zamówienia`\n" +
+            "    JOIN mydb.napoje np ON n.`Napoje_ID.napoju` = np.ID_napoju";
 
     public void initialize() {
         this.dc = new dbConnection();
     }
 
     @FXML
-    public void update(ActionEvent event) throws SQLException {
-        loadSoki(event);
+    public void update() throws SQLException {
+        loadSoki();
+        loadNapoje();
+        loadSokiZam();
+        loadNapojeZam();
     }
 
     @FXML
-    public void loadSoki(ActionEvent event) throws SQLException{
+    public void loadSoki() throws SQLException{
         try {
             Connection con = dbConnection.getConnection();
             this.sokiList = FXCollections.observableArrayList();
@@ -128,6 +141,76 @@ public class HurtowniaController {
         this.soki.setItems(this.sokiList);
     }
 
+    public void loadNapoje() throws SQLException {
+        try {
+            Connection con = dbConnection.getConnection();
+            this.napojeList = FXCollections.observableArrayList();
+
+            ResultSet rs = con.createStatement().executeQuery(sqlNapoje);
+            while (rs.next()) {
+                this.napojeList.add(new NapojeModel(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6)));
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Error"+e);
+        }
+
+        this.idcol1.setCellValueFactory(new PropertyValueFactory<NapojeModel, String>("ID"));
+        this.nazwacol1.setCellValueFactory(new PropertyValueFactory<NapojeModel, String>("nazwa"));
+        this.prodcol1.setCellValueFactory(new PropertyValueFactory<NapojeModel, String>("producent"));
+        this.pojcol1.setCellValueFactory(new PropertyValueFactory<NapojeModel, String>("pojemnosc"));
+        this.cenacol1.setCellValueFactory(new PropertyValueFactory<NapojeModel, String>("cena"));
+        this.rodzajcol1.setCellValueFactory(new PropertyValueFactory<NapojeModel, String>("rodzaj"));
+
+        this.napoje.setItems(null);
+        this.napoje.setItems(this.napojeList);
+    }
+
+    @FXML
+    public void loadSokiZam() throws SQLException {
+        try {
+            Connection con = dbConnection.getConnection();
+            this.zamSokiList = FXCollections.observableArrayList();
+
+            ResultSet rs = con.createStatement().executeQuery(sqlZamSoki);
+            while (rs.next()) {
+                this.zamSokiList.add(new ZamSokiModel(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5)));
+                }
+            }  catch (SQLException e) {
+            System.err.println("Error"+e);
+        }
+        this.imiecol.setCellValueFactory(new PropertyValueFactory<ZamSokiModel, String>("imie"));
+        this.nazcol.setCellValueFactory(new PropertyValueFactory<ZamSokiModel, String>("nazwisko"));
+        this.sokcol.setCellValueFactory(new PropertyValueFactory<ZamSokiModel, String>("sok"));
+        this.ilecol.setCellValueFactory(new PropertyValueFactory<ZamSokiModel, String>("ilosc"));
+        this.kosztcol.setCellValueFactory(new PropertyValueFactory<ZamSokiModel, String>("koszt"));
+
+        this.zamsoki.setItems(null);
+        this.zamsoki.setItems(this.zamSokiList);
+    }
+
+    @FXML
+    public void loadNapojeZam() throws SQLException {
+        try {
+            Connection con = dbConnection.getConnection();
+            this.zamNapojeList = FXCollections.observableArrayList();
+
+            ResultSet rs = con.createStatement().executeQuery(sqlZamNapoje);
+            while (rs.next()) {
+                this.zamNapojeList.add(new ZamNapojeModel(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5)));
+            }
+        }  catch (SQLException e) {
+            System.err.println("Error"+e);
+        }
+        this.imiecol1.setCellValueFactory(new PropertyValueFactory<ZamNapojeModel, String>("imie"));
+        this.nazcol1.setCellValueFactory(new PropertyValueFactory<ZamNapojeModel, String>("nazwisko"));
+        this.napojcol.setCellValueFactory(new PropertyValueFactory<ZamNapojeModel, String>("napoj"));
+        this.ilecol1.setCellValueFactory(new PropertyValueFactory<ZamNapojeModel, String>("ilosc"));
+        this.kosztcol1.setCellValueFactory(new PropertyValueFactory<ZamNapojeModel, String>("koszt"));
+
+        this.zamnapoje.setItems(null);
+        this.zamnapoje.setItems(this.zamNapojeList);
+    }
     @FXML
     public void insertData() {
         try {
@@ -155,4 +238,5 @@ public class HurtowniaController {
     public void deleteData() {
 
     }
+
 }
